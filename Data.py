c5e1b88647a0ab0201f77c6c096ddc47d2cf7501
Hereisjoyce <Hereisjoyce@users.noreply.github.com>
2016-03-15 10:41:31 +0800

    Added files via upload

---
+++ Data.py
@@ -0,0 +1,37 @@
+__author__ = 'Joyce'
+# coding: utf-8
+# print('I\'m ok')
+# print('I\'m learning \nPy')
+# print('\\\n\\')
+
+# print('\\\t\\')
+# print(r'\\\t\\')
+
+# print('''line1
+# line2
+# line3''')
+
+# print(r'''line1
+# line2
+# line3''')
+
+# a = 'ABC'
+# b = a
+# a = 'XYZ'
+# print b
+# print a
+
+# print 10/3
+# print 10.0/3
+# print 10%3
+
+# print len(u'ABC')
+# print len(u'中文')
+
+print 'Hello, %s' % 'joyce'
+print 'Hi, %s, you have $%d' % ('Michael', 1000000)
+print '%2d-%02d' % (3,1)
+print '%.2f' % 3.1415926
+print 'Age: %s. Gender: %s' % (25, True)
+print u'Hi, %s' % u'Michael'
+print 'Growth rate: %d %%' % 7
